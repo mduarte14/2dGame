bd3569b85cee530e1300b16d9364e9ea8cd2735e
Miguel Duarte <duartemiguel04@gmail.com>
2024-08-23 11:44:27 -0700

    Made a heyo interactive site!

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `Welcome to the interactive Heyo site, here is a link to our corporate site <a href="https://heyo.is/" target="_blank">Heyo</a>!`,
+  wallPicture: `These are our list of Heyo Achievements. I hung it on the wall because I'm proud of it. Hire us!`,
+  "sofa-table": `That's my sofa. I chill here`,
+  tv: `Hmmm? Heyo Superbowl ad? Looks like they know how build really cool sites`,
+  bed: `This where I sleep. ZZZZzzZZZ`,
+  portfolio: `This is our desk we do a lot for brands here.
+    Contact us at corp@heyo.is if you want cool apps like this for your brand!`,
+  projects: `Info about this project : It's made with the Kaboom.js library which is a library for making games in JavaScript. We get to build cool projects like this on fridays.`,
+  library: `Wow, so many books. Seems like the Heyo team knows how to do everything.`,
+  exit: `If you want to exit Heyo's interactive site, just close the tab.`,
+};
